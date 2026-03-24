de4a17b36ef56bbe92365417c0035255474bbda9
Pavel Kononenkov <pavel.kononenkov@convergent.digital>
2026-03-24 13:37:44 +0300

    Set up the project build and deployment to Github Pages.

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -7,7 +7,7 @@ import Tween from "../pages/gsap/Tween";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/react-animation-libraries",
     Component: App,
   },
   {
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+export default defineConfig(({ mode }) => {
+  const isProd = mode === "production";
+  return {
+    base: isProd ? "/react-animation-libraries/" : "/",
+    plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  };
+});
